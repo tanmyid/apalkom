f7ddcd1d7a6c90e1c01a9bde0353c3d3dba42c91
tanmyid <tanmyid1337@gmail.com>
2023-12-26 11:04:32 +0700

    new dashboard

--- index.php
+++ index.php
@@ -28,14 +28,14 @@ include 'layouts/sidebar.php';
                                 <!-- small box -->
                                 <div class="small-box bg-info">
                                     <div class="inner">
-                                        <h3>150</h3>
+                                        <h3><?= $count_nama_aset; ?></h3>
 
-                                        <p>New Orders</p>
+                                        <p>Jenis Aset</p>
                                     </div>
                                     <div class="icon">
                                         <i class="ion ion-bag"></i>
                                     </div>
-                                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                                    <a href="<?= $baseURL; ?>/nama_aset" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                                 </div>
                             </div>
                             <!-- ./col -->
@@ -43,14 +43,14 @@ include 'layouts/sidebar.php';
                                 <!-- small box -->
                                 <div class="small-box bg-success">
                                     <div class="inner">
-                                        <h3>53<sup style="font-size: 20px">%</sup></h3>
+                                        <h3><?= $count_data_aset; ?></h3>
 
-                                        <p>Bounce Rate</p>
+                                        <p>Total Aset</p>
                                     </div>
                                     <div class="icon">
                                         <i class="ion ion-stats-bars"></i>
                                     </div>
-                                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                                    <a href="<?= $baseURL; ?>/data_aset" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                                 </div>
                             </div>
                             <!-- ./col -->
@@ -58,14 +58,14 @@ include 'layouts/sidebar.php';
                                 <!-- small box -->
                                 <div class="small-box bg-warning">
                                     <div class="inner">
-                                        <h3>44</h3>
+                                        <h3><?= $count_reparasi; ?></h3>
 
-                                        <p>User Registrations</p>
+                                        <p>Reparasi</p>
                                     </div>
                                     <div class="icon">
                                         <i class="ion ion-person-add"></i>
                                     </div>
-                                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                                    <a href="<?= $baseURL; ?>/reparasi" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                                 </div>
                             </div>
                             <!-- ./col -->
@@ -73,14 +73,14 @@ include 'layouts/sidebar.php';
                                 <!-- small box -->
                                 <div class="small-box bg-danger">
                                     <div class="inner">
-                                        <h3>65</h3>
+                                        <h3><?= $count_pemusnahan; ?></h3>
 
-                                        <p>Unique Visitors</p>
+                                        <p>Pemusnahan</p>
                                     </div>
                                     <div class="icon">
                                         <i class="ion ion-pie-graph"></i>
                                     </div>
-                                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                                    <a href="<?= $baseURL; ?>/pemusnahan" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                                 </div>
                             </div>
                             <!-- ./col -->
